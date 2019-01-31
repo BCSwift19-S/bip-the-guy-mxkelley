fe53e5f4c27c323a6e822a6c1eb5f07a37d4ab6f
Mr. Michael Kelley <mx@kell4.com>
2019-01-31 10:47:05 -0500

    Initial Commit

---
+++ Bip The Guy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bip The Guy
+//
+//  Created by Michael X Kelley on 1/31/19.
+//  Copyright © 2019 Michael X Kelley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
